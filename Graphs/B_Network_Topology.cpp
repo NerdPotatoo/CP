5c0bf273db833df4cc718a884400eb44c591e5e8
Yasir Adnan <134677437+NerdPotatoo@users.noreply.github.com>
2025-07-18 17:09:16 +0000

    Learning Graph

---
+++ Graphs/B_Network_Topology.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fastIO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+template <typename T> using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+const int mod = 1e9+7;
+
+void solve()
+{
+    // CODE HERE
+    int n,m;
+    cin >> n >> m;
+    vector<int> a[n+1];
+    while(m--)
+    {
+        int u,v;
+        cin >> u >> v;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    set<int> s;
+    for(int i = 1; i<=n; i++)
+    {
+        s.insert(a[i].size());
+    }
+    // cout << s.size() << '\n';
+    if(s.size() == 1 && *s.begin() == 2)
+    {
+        cout << "ring topology\n";
+        return;
+    }
+    if(s.size() == 2 && (*s.begin() == 1 && *s.rbegin() == n-1))
+    {
+        cout << "star topology\n";
+        return;
+    }
+    if(s.size() == 2 && (*s.begin() == 1 && *s.rbegin() == 2))
+    {
+        cout << "bus topology\n";
+        return;
+    }
+
+    cout << "unknown topology\n";
+}
+int32_t main()
+{
+    fastIO;
+    int T = 1;
+    // cin>>T;
+    for(int cs = 1; cs <= T; cs++)
+    {
+        // cout<<"Case "<<cs<< ": "<<solve();
+        solve();
+    }
+    return 0;
+}
