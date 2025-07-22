41805f69fae4540ef1197299c62729770ee0f74b
Yasir Adnan <134677437+NerdPotatoo@users.noreply.github.com>
2025-07-22 17:23:18 +0000

    EDU 181

---
+++ Codeforces/B_Left_and_Down.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fastIO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+template <typename T> using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+void solve()
+{
+    // CODE HERE
+    ll a, b, k;
+    cin >> a >> b  >> k;
+
+    ll g = __gcd(a,b);
+
+    a /= g;
+    b /= g;
+
+    if(a<=k && b<=k) cout << 1 << '\n';
+    else cout << 2 << '\n';
+}
+int32_t main()
+{
+    fastIO;
+    int T = 1;
+    cin>>T;
+    for(int cs = 1; cs <= T; cs++)
+    {
+        // cout<<"Case "<<cs<< ": "<<solve();
+        solve();
+    }
+    return 0;
+}
