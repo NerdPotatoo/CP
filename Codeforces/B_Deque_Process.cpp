1036e812ea7bef4b1bfb645bf9f75c2911299afb
Yasir Adnan <yasiradnan2022@gmail.com>
2025-08-08 10:07:03 +0600

    Div2 (1039)

---
+++ Codeforces/B_Deque_Process.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fastIO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+template <typename T> using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+const int mod = 1e9+7;
+
+void solve()
+{
+    // CODE HERE
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto &e:a) cin >> e;
+    int l = 0, r = n-1, k = 0; // k = 0 -> small, k = 1 -> large
+
+    string s = "";
+    while(l<=r)
+    {
+        if(k==0)
+        {
+            if(a[l]<a[r])
+            {
+                s += 'L';
+                l++;
+                
+            }
+            else{
+                s += 'R';
+                r--;
+            }
+            k = 1;
+        }
+        else{
+            if(a[l]>a[r])
+            {
+                s += 'L';
+                l++;
+                
+            }
+            else{
+                s += 'R';
+                r--;
+            }
+            k = 0;
+        }
+    }
+    cout << s << '\n';
+}
+int32_t main()
+{
+    fastIO;
+    int T = 1;
+    cin>>T;
+    for(int cs = 1; cs <= T; cs++)
+    {
+        // cout<<"Case "<<cs<< ": "<<solve();
+        solve();
+    }
+    return 0;
+}
