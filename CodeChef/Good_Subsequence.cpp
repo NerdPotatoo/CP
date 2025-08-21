2148a3baa8f5348726d19fc13d1d92615702adad
Yasir Adnan <yasiradnan2022@gmail.com>
2025-08-21 20:28:44 +0600

    Starter 200

---
+++ CodeChef/Good_Subsequence.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fastIO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+template <typename T> using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+const int mod = 1e9+7;
+
+void solve()
+{
+    // CODE HERE
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto &e:a) cin >> e;
+    int k = 1, cnt = 1;
+    if(a[0]%2==1) k = 0;
+
+    for(int i = 1; i<n; i++)
+    {
+        
+        if(k == 0 && a[i]%2 == 0)
+        {
+            cnt++;
+            k = 1;
+        }
+        if(k == 1 && a[i]&1)
+        {
+            cnt++;
+            k = 0;
+        }
+        // cout << a[i] << " " << k << " " << cnt<< '\n';
+    }
+    // cout << endl;
+    cout << cnt << '\n';
+    
+}
+int32_t main()
+{
+    fastIO;
+    int T = 1;
+    cin>>T;
+    for(int cs = 1; cs <= T; cs++)
+    {
+        // cout<<"Case "<<cs<< ": "<<solve();
+        solve();
+    }
+    return 0;
+}
