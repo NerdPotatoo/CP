2f0d44b2dc8636b5aaa336012e115db6b73f5255
Yasir Adnan <yasiradnan2022@gmail.com>
2025-08-23 00:39:43 +0600

    Div3 (1043)

---
+++ Codeforces/A_Homework.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fastIO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+template <typename T> using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+const int mod = 1e9+7;
+
+void solve()
+{
+    // CODE HERE
+    int n;
+    cin >> n; 
+    string s, D = "", V = "", a, b;
+    cin >> s;
+    int m;
+    cin >> m >> a >> b;
+    for(int i = 0; i<m; i++)
+    {
+        if(b[i] == 'D')
+        {
+            D += a[i];
+        }
+        else{
+            V += a[i];
+        }
+    }
+    reverse(all(V));
+    cout << V << s << D << '\n';
+}
+int32_t main()
+{
+    fastIO;
+    int T = 1;
+    cin>>T;
+    for(int cs = 1; cs <= T; cs++)
+    {
+        // cout<<"Case "<<cs<< ": "<<solve();
+        solve();
+    }
+    return 0;
+}
