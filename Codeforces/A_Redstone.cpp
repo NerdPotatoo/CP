cc6e26074583c7e56c4a98c8f52b85c55872ab6d
Yasir Adnan <yasiradnan2022@gmail.com>
2025-08-26 20:24:17 +0600

    Div2 (1044)

---
+++ Codeforces/A_Redstone.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fastIO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+template <typename T> using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+const int mod = 1e9+7;
+
+void solve()
+{
+    // CODE HERE
+    int n, x;
+    cin >> n;
+    vector<int> a(101);
+    for(int i = 0; i<n; i++)
+    {
+        cin >> x;
+        a[x]++;
+    }
+    if(*max_element(all(a)) >= 2) yes
+    else no
+    
+}
+int32_t main()
+{
+    fastIO;
+    int T = 1;
+    cin>>T;
+    for(int cs = 1; cs <= T; cs++)
+    {
+        // cout<<"Case "<<cs<< ": "<<solve();
+        solve();
+    }
+    return 0;
+}
